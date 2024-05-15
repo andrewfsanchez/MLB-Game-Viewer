db6c1e31ca58c8eff9475047d67eb29d35e44ed2
Andrew Sanchez <andrewfsanchez@gmail.com>
2024-05-15 15:46:51 -0400

    Fix Game List score

--- src/GameList.js
+++ src/GameList.js
@@ -6,7 +6,7 @@ function displayGame(game, onGameSelect){
     return (
         <x.div display="grid" maxWidth={1120} justifyContent="center" ml="auto" mr="auto" pb={2}>
             <x.div boxSizing="border-box" h={35} w="1000" border={1} p={2} display="flex" justifyContent="center" bg="blue-gray-500-a10" onClick={() => onGameSelect(game.gamepk)} cursor="pointer">
-                <div>{game.home} {game.awayScore} @ {game.homeScore} {game.away}</div>
+                <div>{game.away} {game.awayScore} @ {game.homeScore} {game.home}</div>
             </x.div>
         </x.div>
     )
